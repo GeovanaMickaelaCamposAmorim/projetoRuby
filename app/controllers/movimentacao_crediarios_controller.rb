f8b0338368a21ddbdc15deadad58860cee835f8b
Luan Vitor <luanvitors_852@hotmail.com>
2025-11-26 02:28:16 -0300

    Atualização na movimentação de crediário, controle de crediário e rotas

--- app/controllers/movimentacao_crediarios_controller.rb
+++ app/controllers/movimentacao_crediarios_controller.rb
@@ -1,5 +1,6 @@
 class MovimentacaoCrediariosController < ApplicationController
   before_action :set_ficha
+  before_action :set_mov, only: [ :edit, :update, :destroy ]
 
   def index
     @movs = @ficha.movimentacao_crediarios.order(created_at: :desc)
@@ -7,55 +8,98 @@ class MovimentacaoCrediariosController < ApplicationController
 
   def new
     @mov = @ficha.movimentacao_crediarios.new
+    render layout: false
+  end
+
+  def edit
+    render layout: false
   end
 
   def create
     @mov = @ficha.movimentacao_crediarios.new(mov_params)
-    @mov.user = current_user  # Preferível ao user_id=
-
-    # Calcular valor líquido (taxa de cartão)
-    if @mov.mov_tipo == "pagamento"
-      taxa = current_contratante.taxa_cartoes.to_f
-      @mov.mov_valor_real = @mov.mov_valor - (@mov.mov_valor * taxa / 100.0)
-    else
-      @mov.mov_valor_real = @mov.mov_valor
-    end
+    @mov.user = current_user
+    calcular_valor_real(@mov)
 
     if @mov.save
       atualizar_ficha(@mov)
-      redirect_to movimentacao_crediarios_path(ficha_crediario_id: @ficha.id),
-                  notice: "Movimentação registrada!"
+      respond_to do |format|
+        format.html { redirect_to movimentacao_crediarios_path(ficha_crediario_id: @ficha.id), notice: "Movimentação registrada!" }
+        format.js { head :ok }
+      end
     else
-      render :new, status: :unprocessable_entity
+      render :new, layout: false, status: :unprocessable_entity
     end
   end
+
+  def update
+    @mov.assign_attributes(mov_params)
+    calcular_valor_real(@mov)
+    if @mov.save
+      atualizar_ficha(@mov)
+      redirect_to movimentacao_crediarios_path(ficha_crediario_id: @ficha.id), notice: "Movimentação atualizada!"
+    else
+      render :edit, layout: false, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  case @mov.mov_tipo
+  when "pagamento"
+    @ficha.update(fic_valor_total: @ficha.fic_valor_total + @mov.mov_valor_real)
+  when "juros", "compra"
+    @ficha.update(fic_valor_total: @ficha.fic_valor_total - @mov.mov_valor)
+  when "devolucao"
+    @ficha.update(fic_valor_total: @ficha.fic_valor_total + @mov.mov_valor)
+  end
+
+  @ficha.update(fic_status: @ficha.fic_valor_total > 0 ? "pendente" : "concluida")
+
+  @mov.destroy
+
+  respond_to do |format|
+    format.html { redirect_to ficha_crediario_movimentacao_crediarios_path(@ficha), notice: "Movimentação excluída." }
+    format.js
+    format.json { head :no_content }
+  end
+  end
+
+
+
+
 
   private
-
-  def atualizar_ficha(mov)
-    case mov.mov_tipo
-    when "pagamento"
-      @ficha.update(fic_valor_total: @ficha.fic_valor_total - mov.mov_valor_real)
-    when "juros"
-      @ficha.update(fic_valor_total: @ficha.fic_valor_total + mov.mov_valor)
-    when "compra"
-      @ficha.update(fic_valor_total: @ficha.fic_valor_total + mov.mov_valor)
-    when "devolucao"
-      @ficha.update(fic_valor_total: @ficha.fic_valor_total - mov.mov_valor)
-    end
-
-    # Atualizar status: concluída se zerado ou negativo
-    @ficha.update(
-      fic_status: @ficha.fic_valor_total > 0 ? "pendente" : "concluida"
-    )
-  end
 
   def set_ficha
     @ficha = FichaCrediario.find(params[:ficha_crediario_id])
   end
 
+  def set_mov
+    @mov = @ficha.movimentacao_crediarios.find(params[:id])
+  end
+
   def mov_params
-    params.require(:movimentacao_crediario)
-          .permit(:mov_tipo, :mov_valor, :mov_observacao)
+    params.require(:movimentacao_crediario).permit(:mov_tipo, :mov_valor, :mov_observacao)
+  end
+
+  def calcular_valor_real(mov)
+    if mov.mov_tipo == "pagamento"
+      taxa = current_contratante.taxa_cartoes.to_f
+      mov.mov_valor_real = mov.mov_valor - (mov.mov_valor * taxa / 100.0)
+    else
+      mov.mov_valor_real = mov.mov_valor
+    end
+  end
+
+  def atualizar_ficha(mov)
+    case mov.mov_tipo
+    when "pagamento"
+      @ficha.update(fic_valor_total: @ficha.fic_valor_total - mov.mov_valor_real)
+    when "juros", "compra"
+      @ficha.update(fic_valor_total: @ficha.fic_valor_total + mov.mov_valor)
+    when "devolucao"
+      @ficha.update(fic_valor_total: @ficha.fic_valor_total - mov.mov_valor)
+    end
+
+    @ficha.update(fic_status: @ficha.fic_valor_total > 0 ? "pendente" : "concluida")
   end
 end
